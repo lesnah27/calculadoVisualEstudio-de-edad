81fbd299027c5b0fb158590612456707f47cd53e
lesnah27 <lesnah27@yahoo.com>
2016-11-13 17:22:03 -0400

    Actualizacion Final

--- Calculadora DeEdad/Form1.Designer.cs
+++ Calculadora DeEdad/Form1.Designer.cs
@@ -175,7 +175,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(120, 114);
+            this.button1.Location = new System.Drawing.Point(116, 115);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(105, 23);
             this.button1.TabIndex = 22;
