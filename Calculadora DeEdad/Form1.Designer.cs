a5fc193b5079e6f264b2ebffc91ccc4d773c373a
lesnah27 <lesnah27@yahoo.com>
2016-11-13 16:44:50 -0400

    Add project files.

---
+++ Calculadora DeEdad/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Calculadora_DeEdad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.DTP = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.TxtEdad = new System.Windows.Forms.TextBox();
+            this.TxtMeses = new System.Windows.Forms.TextBox();
+            this.TxtDias = new System.Windows.Forms.TextBox();
+            this.TxtHoras = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtMinutos = new System.Windows.Forms.TextBox();
+            this.TxtSegundo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TxtDiaNac = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Fecha de Nacimeinto:";
+            // 
+            // DTP
+            // 
+            this.DTP.Location = new System.Drawing.Point(177, 28);
+            this.DTP.Name = "DTP";
+            this.DTP.Size = new System.Drawing.Size(200, 20);
+            this.DTP.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(217, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "-------------Resultado--------------";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "EDAD";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(52, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "MESES";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(52, 276);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "DIAS";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(51, 302);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "HORAS";
+            // 
+            // TxtEdad
+            // 
+            this.TxtEdad.Location = new System.Drawing.Point(103, 217);
+            this.TxtEdad.Name = "TxtEdad";
+            this.TxtEdad.Size = new System.Drawing.Size(32, 20);
+            this.TxtEdad.TabIndex = 13;
+            // 
+            // TxtMeses
+            // 
+            this.TxtMeses.Location = new System.Drawing.Point(103, 243);
+            this.TxtMeses.Name = "TxtMeses";
+            this.TxtMeses.Size = new System.Drawing.Size(100, 20);
+            this.TxtMeses.TabIndex = 14;
+            // 
+            // TxtDias
+            // 
+            this.TxtDias.Location = new System.Drawing.Point(103, 269);
+            this.TxtDias.Name = "TxtDias";
+            this.TxtDias.Size = new System.Drawing.Size(100, 20);
+            this.TxtDias.TabIndex = 15;
+            // 
+            // TxtHoras
+            // 
+            this.TxtHoras.Location = new System.Drawing.Point(103, 295);
+            this.TxtHoras.Name = "TxtHoras";
+            this.TxtHoras.Size = new System.Drawing.Size(100, 20);
+            this.TxtHoras.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 328);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "MINUTOS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 354);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "SEGUNDOS";
+            // 
+            // TxtMinutos
+            // 
+            this.TxtMinutos.Location = new System.Drawing.Point(103, 321);
+            this.TxtMinutos.Name = "TxtMinutos";
+            this.TxtMinutos.Size = new System.Drawing.Size(100, 20);
+            this.TxtMinutos.TabIndex = 20;
+            // 
+            // TxtSegundo
+            // 
+            this.TxtSegundo.Location = new System.Drawing.Point(103, 347);
+            this.TxtSegundo.Name = "TxtSegundo";
+            this.TxtSegundo.Size = new System.Drawing.Size(100, 20);
+            this.TxtSegundo.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Calcula Tu edad";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtDiaNac
+            // 
+            this.TxtDiaNac.Location = new System.Drawing.Point(171, 181);
+            this.TxtDiaNac.Name = "TxtDiaNac";
+            this.TxtDiaNac.Size = new System.Drawing.Size(100, 20);
+            this.TxtDiaNac.TabIndex = 23;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(138, 224);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(37, 13);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "AÑOS";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(52, 188);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 13);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "DIA DE NACIMIENTO";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(43, 407);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(122, 13);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "ING.HANSEL DE LEON";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 426);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.TxtDiaNac);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtSegundo);
+            this.Controls.Add(this.TxtMinutos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtHoras);
+            this.Controls.Add(this.TxtDias);
+            this.Controls.Add(this.TxtMeses);
+            this.Controls.Add(this.TxtEdad);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DTP);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CALCULADORA DE EDAD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker DTP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox TxtEdad;
+        private System.Windows.Forms.TextBox TxtMeses;
+        private System.Windows.Forms.TextBox TxtDias;
+        private System.Windows.Forms.TextBox TxtHoras;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtMinutos;
+        private System.Windows.Forms.TextBox TxtSegundo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TxtDiaNac;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Calculadora DeEdad/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_DeEdad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeSpan Diferencia = DateTime.Today.Subtract(DTP.Value.Date);
+            int Edad = (int)(Diferencia.TotalDays / 365.25);
+            TxtMeses.Text= DTP.Value.Month.ToString();
+           
+            TxtDias.Text = Diferencia.TotalDays.ToString();
+            TxtHoras.Text = Diferencia.TotalHours.ToString();
+            TxtMinutos.Text = Diferencia.TotalMinutes.ToString();
+            TxtSegundo.Text = Diferencia.TotalSeconds.ToString();
+            TxtEdad.Text = Edad.ToString();
+            System.DateTime Fnac = DTP.Value.Date;
+            switch (Fnac.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    TxtDiaNac.Text = "Lunes";
+                    break;
+                case DayOfWeek.Tuesday:
+                    TxtDiaNac.Text = "Martes";
+                    break;
+                case DayOfWeek.Wednesday:
+                    TxtDiaNac.Text = "Miercoles";
+                    break;
+                case DayOfWeek.Thursday:
+                    TxtDiaNac.Text = "Jueves";
+                    break;
+                case DayOfWeek.Friday:
+                    TxtDiaNac.Text = "Viernes";
+                    break;
+                case DayOfWeek.Saturday:
+                    TxtDiaNac.Text = "Sabado";
+                    break;
+                case DayOfWeek.Sunday:
+                    TxtDiaNac.Text = "Domingo";
+                    break;
+            }
+            }
+    }
+}
